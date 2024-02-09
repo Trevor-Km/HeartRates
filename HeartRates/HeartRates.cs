f358cc79f8b228da065e36ddc7c16a5506f1b266
trevo <you@example.com>
2024-02-08 22:43:23 -0500

    Add project files.

---
+++ HeartRates/HeartRates.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartRates
+{
+    internal class HeartRates
+    {
+        private string firstName;
+        private string lastName;
+        private int birthYear;
+        private int currentYear;
+
+
+        public HeartRates(string firstName, string lastName, int birthYear, int currentYear)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.BirthYear = birthYear;
+            this.CurrentYear = currentYear;
+        }
+
+        public HeartRates()
+        {
+        }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public int BirthYear
+        {
+            get
+            {
+                return birthYear;
+            }
+            set
+            {
+                birthYear = value;
+            }
+        }
+        public int CurrentYear
+        {
+            get
+            {
+                return currentYear;
+            }
+            set
+            {
+                currentYear = value;
+            }
+        }
+
+        public int CalcAge
+        {
+            get
+            {
+                return currentYear - birthYear;
+            }
+        }
+
+        public int CalcMaxHR
+        {
+            get
+            {
+                return 220 - CalcAge;
+            }
+        }
+
+        public double MinTargetHR
+        {
+            get
+            {
+                return CalcMaxHR * 0.5;
+            }
+        }
+
+        public double MaxTargetHR
+        {
+            get
+            {
+                return CalcMaxHR * 0.85;
+            }
+        }
+
+        private int CalculateAge(int birthYear, int currentYear)
+        {
+            return currentYear - birthYear;
+
+        }
+        private int CalculateHR(int birthYear, int currentYear)
+        {
+            int age = CalculateAge(birthYear, currentYear);
+            return 220 - age;
+
+        }
+
+
+        private string CalculateTargetRange(int birthYear, int currentYear)
+        {
+
+            int heartRate = CalculateHR(birthYear, currentYear);
+            double target1 = heartRate * 0.5;
+            double target2 = heartRate * 0.85;
+            return $"{target1} - {target2} ";
+
+        }
+
+        public void displayPatientRecord()
+        {
+            drawLine();
+            Console.WriteLine($"|  {"PATIENT HEART RATE RECORDS",53} {"|",25}");
+            drawLine();
+            Console.WriteLine($"| {"Patient Name ",-25} | {lastName + ", " + firstName,-50} |");
+            drawLine();
+            Console.WriteLine($"| {"Patient Birth Year ",-25} | {birthYear,50} |");
+            drawLine();
+            Console.WriteLine($"| {"Patient Age ",-25} | {CalcAge,50} |");
+            drawLine();
+            Console.WriteLine($"| {"Maximum Heart Rate ",-25} | {CalcMaxHR,50} |");
+            drawLine();
+            Console.WriteLine($"| {"Target Heart Rate Range ",-25} | {MinTargetHR + " - " + MaxTargetHR,50} |");
+            drawLine();
+
+        }
+
+        public static void drawLine()
+        {
+            Console.Write("|");
+            for (int i = 0; i < 80; i++)
+
+                Console.Write("-");
+            Console.WriteLine("|");
+
+
+        }
+
+    }
+}
---
+++ HeartRates/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartRates
+{
+    internal class Program
+    {
+               static void Main(string[] args)
+        {
+
+            
+            
+            
+            HeartRates hr1 = new HeartRates();
+            Console.WriteLine("Please enter your last name");
+            hr1.LastName = Console.ReadLine();
+            Console.WriteLine("Please enter your first name");
+            hr1.FirstName = Console.ReadLine();
+            Console.WriteLine("Please enter your birth year");
+            hr1.BirthYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the current year");
+            hr1.CurrentYear = int.Parse(Console.ReadLine());
+
+            hr1.displayPatientRecord();
+
+        }
+    }
+}
